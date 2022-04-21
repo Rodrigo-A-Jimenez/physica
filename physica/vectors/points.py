df21187ad62021a8e096f8d4bba6852987c454bd
Rodrigo J <jimenezhuancarodrigo@gmail.com>
2022-04-21 14:33:31 -0400

    methods get and setter for Point's coords

--- physica/vectors/points.py
+++ physica/vectors/points.py
@@ -1,6 +1,22 @@
 
 class Point:
-    def __init__(self,*args, **kwargs) -> None:
+    def __init__(self,*args, **kwargs) -> None:    
         self.__coordinates = [*args]
+        self.__dim = len(self.__coordinates)
+    
+    def __len__(self) -> int:
+        return self.__dim
+    
+    @property
+    def coordinates(self) -> list:
+        return self.__coordinates
+    
+    @property
+    def dim(self) -> int:
+        return self.__dim
+    
+    @coordinates.setter
+    def coordinates(self, new_coordinates: list) -> None:
+        self.__coordinates = new_coordinates
         self.__dim = len(self.__coordinates)
         
--- physica/vectors/tests/test_point.py
+++ physica/vectors/tests/test_point.py
@@ -4,3 +4,14 @@ def test_coord():
     A = Point(1,2,3)
     assert A.coordinates == [1,2,3]
     
+    A.coordinates = [4,5,6]
+    assert A.coordinates == [4,5,6]
+    
+    
+def test_dim():
+    A = Point(1,2,3)
+    assert A.dim == 3
+    
+    A.coordinates = [4,5]
+    assert A.dim == 2
+
